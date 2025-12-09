a3f71afe89b9b106c4be939950e645cb4f019f97
wuqui <q.wuerschinger@gmail.com>
2025-12-10 00:05:44 +0100

    Add section-level removal for headers with .excl class
    Headers with .excl now remove the entire section including all content
    and nested subsections until the next same-level or higher header.
    
    Uses pandoc.utils.make_sections() to create hierarchical structure,
    then removes section divs whose header has .excl class.

--- _extensions/exclude/exclude.lua
+++ _extensions/exclude/exclude.lua
@@ -10,13 +10,19 @@
 --   [inline content]{.excl}
 --
 --   ## Header {.excl}
+--   Content below header is also excluded (entire section)
 --
 -- Control visibility in YAML frontmatter:
 --   params:
 --     show_excl: false  # hide .excl content
 
+-- Requires pandoc 2.8+ for pandoc.utils.make_sections
+PANDOC_VERSION:must_be_at_least {2,8}
+
+local utils = require 'pandoc.utils'
 local show_excl = true  -- default: show everything
 
+-- Helper: check if element has class
 local function has_class(el, name)
   for _, class in ipairs(el.classes) do
     if class == name then
@@ -26,6 +32,24 @@ local function has_class(el, name)
   return false
 end
 
+-- Helper: check if div is a section div created by make_sections
+local function is_section_div(div)
+  return div.t == 'Div'
+    and div.classes[1] == 'section'
+    and div.attributes.number
+end
+
+-- Helper: get header from section div
+local function section_header(div)
+  if not is_section_div(div) then return nil end
+  local header = div.content and div.content[1]
+  if header and header.t == 'Header' then
+    return header
+  end
+  return nil
+end
+
+-- Pass 1: Read params from document metadata
 function Meta(meta)
   if meta.params and meta.params.show_excl == false then
     show_excl = false
@@ -33,28 +57,50 @@ function Meta(meta)
   return meta
 end
 
-function Div(el)
-  if has_class(el, "excl") and not show_excl then
-    return {}
-  end
-  return el
+-- Pass 2: Setup - wrap all sections in Div elements
+local function setup_document(doc)
+  if show_excl then return nil end  -- skip processing if showing everything
+  local sections = utils.make_sections(false, nil, doc.blocks)
+  return pandoc.Pandoc(sections, doc.meta)
 end
 
-function Span(el)
-  if has_class(el, "excl") and not show_excl then
+-- Pass 3: Remove section divs where header has .excl class
+local function drop_excl_sections(div)
+  if show_excl then return nil end
+  local header = section_header(div)
+  if header and has_class(header, "excl") then
     return {}
   end
-  return el
 end
 
-function Header(el)
+-- Pass 4: Flatten remaining section divs back to normal structure
+local function flatten_sections(div)
+  if show_excl then return nil end
+  local header = section_header(div)
+  if not header then return nil end
+  -- Preserve the section identifier on the header
+  header.identifier = div.identifier
+  div.content[1] = header
+  return div.content
+end
+
+-- Pass 5: Remove standalone .excl divs and spans
+local function remove_excl_div(el)
   if has_class(el, "excl") and not show_excl then
     return {}
   end
-  return el
+end
+
+local function remove_excl_span(el)
+  if has_class(el, "excl") and not show_excl then
+    return {}
+  end
 end
 
 return {
   { Meta = Meta },
-  { Div = Div, Span = Span, Header = Header }
+  { Pandoc = setup_document },
+  { Div = drop_excl_sections },
+  { Div = flatten_sections },
+  { Div = remove_excl_div, Span = remove_excl_span }
 }
